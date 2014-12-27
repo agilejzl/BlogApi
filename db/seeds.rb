a5622fa4549893ea5c31549c9545692daf862f2e
Teddy Jiang <agilejzl@gmail.com>
2014-12-27 17:03:36 +0800

    set no seed for travis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 if Article.count == 0
   author1 = Author.find_or_create_by(:name => 'author')
-  author2 = Author.find_or_create_by(:name => 'author2') 
+  author2 = Author.find_or_create_by(:name => 'author2')
   Article.create([
     { :title => 'title', :content => 'content', :author_id => author1.id },
     { :title => 'title2', :content => 'content2', :author_id => author2.id }
